14b1bef8e4da6ca5ad5ef430aeb68a7f700205b1
Lucabruu1 <59821064+Lucabruu1@users.noreply.github.com>
2025-07-17 17:05:34 +0200

    app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from scipy.io import wavfile
 import io
 
-<a href="https://kydrfedz3fbzz3menrwhyy.streamlit.app/" a/>
+<a href="https://kydrfedz3fbzz3menrwhyy.streamlit.app" a/>
 
 
 
